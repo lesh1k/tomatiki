a7c45e21a81096150f4bdca77fabe93d1f63ba83
Alexei Dvorac <lexhxas@gmail.com>
2016-03-23 12:17:15 +0200

    Implement simple, stupid JS countdown timer.

--- client/timer.js
+++ client/timer.js
@@ -1,4 +1,6 @@
 (function() {
+    'use strict';
+
     var TIMER_DEFAULTS = {
         work: {
             hours: 0,
@@ -22,10 +24,61 @@
         }
     };
 
+    class Timer {
+
+        constructor(duration, interval=1) {
+            this.duration = duration; // seconds
+            this.interval = interval; // seconds
+            this.secondsToTimer();
+        }
+
+        secondsToTimer() {
+            const seconds_in_minute = 60,
+                seconds_in_hour = 3600;
+
+            this.hours = Math.floor(this.duration / seconds_in_hour);
+            this.minutes = Math.floor((this.duration - this.hours * seconds_in_hour) / seconds_in_minute);
+            this.seconds = this.duration % seconds_in_minute;
+        }
+
+        start() {
+            this.interval_id = window.setInterval(this.countdown.bind(this), 1000 * this.interval);
+        }
+
+        stop() {
+            window.clearInterval(this.interval_id);
+        }
+
+        countdown() {
+            if (!this.seconds && !this.minutes && !this.hours) {
+                this.stop()
+            } else if (this.seconds > 0) {
+                this.seconds -= 1;
+            } else if (this.seconds === 0 && this.minutes > 0) {
+                this.minutes -= 1;
+                this.seconds = 59;
+            } else if (this.minutes === 0 && this.hours > 0) {
+                this.hours -= 1;
+                this.minutes = 59;
+                this.seconds = 59;
+            }
+
+            this.updateSession();
+        }
+
+        updateSession() {
+            Session.set('hours', this.hours);
+            Session.set('minutes', this.minutes);
+            Session.set('seconds', this.seconds);
+        }
+    }
+
+    const TIMER = new Timer(25*60);
+
     Template.timer.helpers({
-        hours: function() { return Session.get('hours') || TIMER_DEFAULTS.work.hours; },
-        minutes: function() { return Session.get('minutes') || TIMER_DEFAULTS.work.minutes; },
-        seconds: function() { return Session.get('seconds') || TIMER_DEFAULTS.work.seconds; },
+        hours: function() { return Session.get('hours') || TIMER.hours; },
+        minutes: function() { return Session.get('minutes') || TIMER.minutes; },
+        seconds: function() { return Session.get('seconds') || TIMER.seconds; },
         timer_separator: ':',
         timer_state: function() { return Session.get('timer_state') || 'stopped'; },
         button_label: function() { return Session.get('button_label') || 'Start'; }
@@ -33,8 +86,16 @@
 
     Template.timer.events({
         'click .timer__trigger': function(event, template) {
-            Session.set('button_label', 'Stop');
-            Session.set('timer_state', 'running');
+            if (Session.get('timer_state') === 'stopped') {
+                TIMER.start();
+                Session.set('button_label', 'Stop');
+                Session.set('timer_state', 'running');
+            } else {
+                TIMER.stop();
+                Session.set('button_label', 'Start');
+                Session.set('timer_state', 'stopped');
+            }
         }
     });
+
 })();
