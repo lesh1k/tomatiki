9d167734df7646ff34ef9b4ad325e0d56a610bd1
Alexei Dvorac <lexhxas@gmail.com>
2016-03-23 20:55:47 +0200

    Add a few tests for utils-leftpad.

--- packages/utils-leftpad/utils-leftpad-tests.js
+++ packages/utils-leftpad/utils-leftpad-tests.js
@@ -1,5 +1,21 @@
 // Write your tests here!
-// Here is an example.
-Tinytest.add('example', function (test) {
-  test.equal(true, true);
+Tinytest.add('Output values', function(test) {
+    test.equal(leftPad(''), '00');
+    test.equal(leftPad(1), '01');
+    test.equal(leftPad('1'), '01');
+    test.equal(leftPad(11), '11');
+    test.equal(leftPad(11, 10), '0000000011');
+    test.equal(leftPad(1, 3, 'A'), 'AA1');
+    test.equal(leftPad(11, 10, 'A'), 'AAAAAAAA11');
+    test.equal(leftPad(123456789, 10, 'A'), 'A123456789');
+    test.equal(leftPad(1234567890, 10, 'A'), '1234567890');
+});
+
+Tinytest.add('Argumets types', function(test) {
+    let sample_space = [[], {}, Symbol(), void 0, null, NaN];
+    for (let value of sample_space) {
+        test.throws(leftPad(value), TypeError);
+    }
+
+    test.throws(leftPad(1, 1, {}), TypeError);
 });
