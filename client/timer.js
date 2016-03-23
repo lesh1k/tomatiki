6a648d655919d5260e3d767d9c4b5feef01a42b7
Alexei Dvorac <lexhxas@gmail.com>
2016-03-23 10:42:08 +0200

    Add timer template with sample helpers and dummy timer start action.

---
+++ client/timer.js
@@ -0,0 +1,41 @@
+(function() {
+    var TIMER_DEFAULTS = {
+        work: {
+            hours: 0,
+            minutes: 25,
+            seconds: 0
+        },
+        break: {
+            hours: 0,
+            minutes: 5,
+            seconds: 0
+        },
+        long_break: {
+            hours: 0,
+            minutes: 15,
+            seconds: 0
+        },
+        state: {
+            stopped: 'stopped',
+            running: 'running',
+            paused: 'paused'
+        }
+    };
+
+    Template.timer.helpers({
+        hours: function() { return Session.get('hours') || TIMER_DEFAULTS.work.hours; },
+        minutes: function() { return Session.get('minutes') || TIMER_DEFAULTS.work.minutes; },
+        seconds: function() { return Session.get('seconds') || TIMER_DEFAULTS.work.seconds; },
+        timer_separator: ':',
+        timer_state: function() { return Session.get('timer_state') || 'stopped'; },
+        button_label: function() { return Session.get('button_label') || 'Start'; }
+    });
+
+    Template.timer.events({
+        'click .timer__trigger': function(event, template) {
+            debugger
+            Session.set('button_label', 'Stop');
+            Session.set('timer_state', 'running');
+        }
+    });
+})();
