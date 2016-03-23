4f05284c0f69e71628b563270bd2430318ae3564
Alexei Dvorac <lexhxas@gmail.com>
2016-03-23 16:22:09 +0200

    Create leshik:utils-left-pad package.

---
+++ packages/utils-left-pad/utils-left-pad.js
@@ -0,0 +1,17 @@
+// Write your package code here!
+(function() {
+    'use strict';
+
+    function leftPad(primitive, width=2, padder='0') {
+        if (typeof primitive !== 'number' && typeof primitive !== 'string') {
+            throw TypeError(`Expected type "number" OR "string". Received ${typeof primitive}`);
+        }
+
+        let str = String(primitive);
+        while (str.length < width) {
+            str = padder + str;
+        }
+
+        return str;
+    }
+})();
