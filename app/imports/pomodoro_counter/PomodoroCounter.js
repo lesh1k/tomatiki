4a3e7fa9f66c9fce32fdc7212e053638511bea18
Alexei Dvorac <lexhxas@gmail.com>
2016-04-06 21:34:59 +0300

    Add increment/decrement methods to PomodoroCounter.

--- app/imports/pomodoro_counter/PomodoroCounter.js
+++ app/imports/pomodoro_counter/PomodoroCounter.js
@@ -1,7 +1,14 @@
 export class PomodoroCounter {
 
     constructor({count=0}={}) {
-        this.count = 0;
+        this.count = count;
     }
 
+    increment({ amount=1 }={}) {
+        this.count += amount;
+    }
+
+    decrement({ amount=1 }={}) {
+        this.count -= amount;
+    }
 }
--- app/imports/pomodoro_counter/_tests/client/pomodoro_counter.tests.js
+++ app/imports/pomodoro_counter/_tests/client/pomodoro_counter.tests.js
@@ -6,15 +6,55 @@ import { PomodoroCounter } from '../../PomodoroCounter.js';
 
 
 describe('PomodoroCounter', () => {
+
     describe('PomodoroCounter.constructor', () => {
+
         it('Creates a PomodoroCounter instance', () => {
             let p_counter = new PomodoroCounter();
             chai.assert.instanceOf(p_counter, PomodoroCounter);
         });
 
-        it('Sets count to 0, by default', () => {
+        it('Sets PomodoroCounter.count to 0, by default', () => {
             let p_counter = new PomodoroCounter();
             chai.assert.strictEqual(p_counter.count, 0);
         });
+
+        it('Sets PomodoroCounter.count to the supplied value', () => {
+            let p_counter = new PomodoroCounter({ count: 5 });
+            chai.assert.strictEqual(p_counter.count, 5);
+        });
+
     });
+
+    describe('PomodoroCounter.increment', () => {
+
+        it('Increments PomodoroCounter.count by 1 by default', () => {
+            let p_counter = new PomodoroCounter();
+            p_counter.increment();
+            chai.assert.strictEqual(p_counter.count, 1);
+        });
+
+        it('Increments PomodoroCounter.count with the supplied value', () => {
+            let p_counter = new PomodoroCounter();
+            p_counter.increment({ amount: 7 });
+            chai.assert.strictEqual(p_counter.count, 7);
+        });
+    });
+
+    describe('PomodoroCounter.decrement', () => {
+
+        it('Decrements PomodoroCounter.count by 1 by default', () => {
+            let p_counter = new PomodoroCounter({ count: 5 });
+            p_counter.decrement();
+            chai.assert.strictEqual(p_counter.count, 4);
+        });
+
+        it('Decrements PomodoroCounter.count with the supplied value', () => {
+            let p_counter = new PomodoroCounter({ count: 5 });
+            p_counter.decrement({ amount: 5 });
+            chai.assert.strictEqual(p_counter.count, 0);
+        });
+
+    });
+
 });
