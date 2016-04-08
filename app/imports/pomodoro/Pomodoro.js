be448ded0694719846a55aed2aed834f1fe16a79
Alexei Dvorac <lexhxas@gmail.com>
2016-04-08 23:22:36 +0300

    Create an instance of Counter and Timer for Pomodoro.

--- app/imports/pomodoro/Pomodoro.js
+++ app/imports/pomodoro/Pomodoro.js
@@ -1,5 +1,17 @@
+import { Timer } from '../timer/Timer.js';
+import { Counter } from '../counter/Counter.js';
+
+
 export class Pomodoro {
     constructor() {
-        
+        this.timer = new Timer({
+            hours: 0,
+            minutes: 25,
+            seconds: 0,
+            miliseconds: 0,
+            interval_ms: 1002 // 1002 to compensate for deviations during countdown
+        });
+
+        this.counter = new Counter({amount: 0});
     }
 }
--- app/imports/pomodoro/_tests/client/pomodoro.tests.js
+++ app/imports/pomodoro/_tests/client/pomodoro.tests.js
@@ -3,6 +3,8 @@
 import { chai } from 'meteor/practicalmeteor:chai';
 
 import { Pomodoro } from '../../Pomodoro.js';
+import { Timer } from '../../../timer/Timer.js';
+import { Counter } from '../../../counter/Counter.js';
 
 
 describe('Pomodoro', () => {
@@ -12,6 +14,16 @@ describe('Pomodoro', () => {
         it('Creates a Pomodoro instance', () => {
             let pomodoro = new Pomodoro();
             chai.assert.instanceOf(pomodoro, Pomodoro);
+        });
+
+        it('Creates an instance of Timer', () => {
+            let pomodoro = new Pomodoro();
+            chai.assert.instanceOf(pomodoro.timer, Timer);
+        });
+
+        it('Creates an instance of Counter', () => {
+            let pomodoro = new Pomodoro();
+            chai.assert.instanceOf(pomodoro.counter, Counter);
         });
 
     });
