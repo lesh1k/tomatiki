2bbdf7801dcdcd3039b49a7fd3eaf40082139c75
Alexei Dvorac <lexhxas@gmail.com>
2016-04-08 23:15:25 +0300

    Add a sample, empty Pomodoro class.

--- app/imports/pomodoro/_tests/client/pomodoro.tests.js
+++ app/imports/pomodoro/_tests/client/pomodoro.tests.js
@@ -1,1 +1,19 @@
 /* eslint-env mocha */
+
+import { chai } from 'meteor/practicalmeteor:chai';
+
+import { Pomodoro } from '../../Pomodoro.js';
+
+
+describe('Pomodoro', () => {
+
+    describe('Pomodoro.constructor', () => {
+
+        it('Creates a Pomodoro instance', () => {
+            let pomodoro = new Pomodoro();
+            chai.assert.instanceOf(pomodoro, Pomodoro);
+        });
+
+    });
+
+});
