47a3ce2bd5459aeb850c1f08a00734d29a67cb01
Alexei Dvorac <lexhxas@gmail.com>
2016-04-12 00:24:51 +0300

    Implement Timer.set and Timer.reset. Change the way Timer.time is set on instantiation.

--- app/imports/timer/Timer.js
+++ app/imports/timer/Timer.js
@@ -9,14 +9,28 @@ const DEFAULTS = {
     state: {
         stopped: 'stopped',
         running: 'running'
-        // completed: 'completed'
-    }
+            // completed: 'completed'
+    },
+    time: {
+        hours: 0,
+        minutes: 25,
+        seconds: 0,
+        miliseconds: 0
+    },
+    interval_ms: 1002
 }
 
 
 export class Timer {
 
-    constructor({hours=0, minutes=25, seconds=0, miliseconds=0, interval_ms=1000, running=false}={}) {
+    constructor({
+        hours = DEFAULTS.time.hours,
+        minutes = DEFAULTS.time.minutes,
+        seconds = DEFAULTS.time.seconds,
+        miliseconds = DEFAULTS.time.miliseconds,
+        interval_ms = DEFAULTS.interval_ms,
+        running = false
+    } = {}) {
         if (interval_ms < 10) {
             throw Error('Interval should be greater or equal to 10ms');
         }
@@ -25,18 +39,32 @@ export class Timer {
         this.interval_ms = interval_ms;
         this.running = running;
         this.error_ms = 0;
-
+        this.duration = 0;
         this.time = new ReactiveDict();
-        this.time.set('hours', hours);
-        this.time.set('minutes', minutes);
-        this.time.set('seconds', seconds);
-        this.time.set('miliseconds', miliseconds);
-
-        this.duration = Timer.getTotalMiliseconds(this.time.all());
-        this.time.set('ms_left', this.duration);
+        this.set({
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+            miliseconds: miliseconds
+        });
     }
 
-    milisecondsToTime(ms=this.duration) {
+    set({
+        hours = 0,
+        minutes = 0,
+        seconds = 0,
+        miliseconds = 0,
+    } = {}) {
+        this.duration = Timer.getTotalMiliseconds({
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+            miliseconds: miliseconds
+        });
+        this.milisecondsToTime(this.duration);
+    }
+
+    milisecondsToTime(ms = this.duration) {
         this.time.set('ms_left', ms);
         this.time.set('hours', Math.floor(ms / DEFAULTS.ms_in_second / DEFAULTS.seconds_in_hour));
         this.time.set(
@@ -49,8 +77,14 @@ export class Timer {
         this.time.set('miliseconds', Math.floor(ms % DEFAULTS.ms_in_second));
     }
 
+    reset() {
+        this.stop();
+        this.is_done = false;
+        this.set({miliseconds: this.duration});
+    }
+
     start() {
-        if(!this.running && this.time.get('ms_left') > 0) {
+        if (!this.running && this.time.get('ms_left') > 0) {
             this.started = new Date().getTime();
             let interval = Math.min(this.time.get('ms_left'), this.interval_ms);
             this.timeout_id = setTimeout(this.countdown.bind(this), interval);
@@ -61,7 +95,7 @@ export class Timer {
     }
 
     stop() {
-        if(!this.running) {
+        if (!this.running) {
             throw new Error('Called Timer.stop() on a stopped instance');
         }
 
@@ -80,7 +114,7 @@ export class Timer {
         } else {
             let expected_interval_ms = this.interval_ms - this.error_ms,
                 actual_interval_ms = this.time.get('ms_left') - ms_left;
-            this.error_ms =  actual_interval_ms - expected_interval_ms;
+            this.error_ms = actual_interval_ms - expected_interval_ms;
 
             let interval = this.interval_ms - this.error_ms;
             if (ms_left < interval) {
@@ -97,11 +131,16 @@ export class Timer {
         this.is_done = true;
     }
 
-    static computeEndDate(delta_ms, now=new Date()) {
+    static computeEndDate(delta_ms, now = new Date()) {
         return new Date(now.getTime() + delta_ms);
     }
 
-    static getTotalMiliseconds({hours=0, minutes=0, seconds=0, miliseconds=0}={}) {
+    static getTotalMiliseconds({
+        hours = 0,
+        minutes = 0,
+        seconds = 0,
+        miliseconds = 0
+    } = {}) {
         let total_ms = 0;
 
         miliseconds && (total_ms += miliseconds);
--- app/imports/timer/_tests/client/timer.tests.js
+++ app/imports/timer/_tests/client/timer.tests.js
@@ -1,8 +1,14 @@
 /* eslint-env mocha */
 
-import { chai } from 'meteor/practicalmeteor:chai';
-import { catchAsync } from '../../../helpers/test-helpers.js';
-import { Timer } from '../../Timer.js';
+import {
+    chai
+} from 'meteor/practicalmeteor:chai';
+import {
+    catchAsync
+} from '../../../helpers/test-helpers.js';
+import {
+    Timer
+} from '../../Timer.js';
 
 
 describe('Timer', () => {
@@ -25,7 +31,11 @@ describe('Timer', () => {
         });
 
         it('Throws an error when interval is set to less than 10ms', () => {
-            chai.assert.throws(() => {new Timer({interval_ms: 9})}, Error);
+            chai.assert.throws(() => {
+                new Timer({
+                    interval_ms: 9
+                })
+            }, Error);
         })
 
         it('Sets up correct default time and running_state values for a new instance', () => {
@@ -69,16 +79,18 @@ describe('Timer', () => {
             });
 
             let expected = 2 * 3600 * 1000 // hours
-            + 34 * 60 * 1000 // minutes
-            + 15 * 1000 // seconds
-            + 888; // miliseconds
+                + 34 * 60 * 1000 // minutes
+                + 15 * 1000 // seconds
+                + 888; // miliseconds
             chai.assert.strictEqual(ms, expected);
         });
     });
 
     describe('Timer.milisecondsToTime', () => {
         it('Updates Timer.time ReactiveDict', () => {
-            let mock = {time: new ReactiveDict()},
+            let mock = {
+                    time: new ReactiveDict()
+                },
                 ms = 123914555; // i.e. 34hrs 25min 14s 555ms
             Timer.prototype.milisecondsToTime.call(mock, ms);
 
@@ -94,7 +106,7 @@ describe('Timer', () => {
         it('Returns a Date object', () => {
             let date_now = new Date('Tue Mar 29 2016 13:50:00 GMT+0300 (EEST)'),
                 delta_ms = 25 * 60 * 1000; // 25 minutes in ms
-                // expected = new Date('Tue Mar 29 2016 14:15:00 GMT+0300 (EEST)');
+            // expected = new Date('Tue Mar 29 2016 14:15:00 GMT+0300 (EEST)');
 
             let actual = Timer.computeEndDate(delta_ms, date_now);
             chai.assert.instanceOf(actual, Date);
@@ -121,32 +133,61 @@ describe('Timer', () => {
 
     describe('Timer.start', () => {
         it('Changes Timer.running to "true"', () => {
-            let timer = new Timer({hours: 1, minutes: 0, seconds: 0, running: false});
+            let timer = new Timer({
+                hours: 1,
+                minutes: 0,
+                seconds: 0,
+                running: false
+            });
             timer.start();
             chai.assert.isTrue(timer.running);
         });
 
         it('Throws an error if called on a running instance.', () => {
-            let timer = new Timer({running: true});
+            let timer = new Timer({
+                running: true
+            });
             chai.assert.throws(Timer.prototype.start.bind(timer), Error);
         });
 
         it('Starts countdown', (done) => {
-            let timer = new Timer({hours: 1, minutes: 0, seconds: 0, interval_ms: 10});
+            // WARNING! When the CPU is overloaded,
+            // asserts (for miliseconds and seconds) might fail
+            // That's not likely but possible.
+            let timer = new Timer({
+                hours: 1,
+                minutes: 0,
+                seconds: 0,
+                interval_ms: 10
+            });
             timer.start();
             setTimeout(() => {
                 timer.stop();
-                catchAsync(done, () => { chai.assert.isFalse(timer.running); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('hours'), 0); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('minutes'), 59); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('seconds'), 59); });
-                catchAsync(done, () => { chai.expect(timer.time.get('miliseconds')).to.be.closeTo(990, TIMER_EPSILON); });
+                catchAsync(done, () => {
+                    chai.assert.isFalse(timer.running);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('hours'), 0);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('minutes'), 59);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('seconds'), 59);
+                });
+                catchAsync(done, () => {
+                    chai.assert.notEqual(timer.time.get('miliseconds'), 0);
+                });
                 done();
             }, 10 + TIMER_EPSILON);
         });
 
         it('Does not change state to running if called when no time left (i.e. 00:00:00)', () => {
-            let timer = new Timer({hours: 0, minutes: 0, seconds: 0});
+            let timer = new Timer({
+                hours: 0,
+                minutes: 0,
+                seconds: 0
+            });
             timer.start();
             chai.assert.isFalse(timer.running);
         });
@@ -154,12 +195,16 @@ describe('Timer', () => {
 
     describe('Timer.stop', () => {
         it('Changes Timer.running to "false"', () => {
-            let timer = new Timer({running: false});
+            let timer = new Timer({
+                running: false
+            });
             chai.assert.throws(Timer.prototype.stop.bind(timer), Error);
         });
 
         it('Throws an error if called on a stopped instance.', () => {
-            let timer = new Timer({running: false});
+            let timer = new Timer({
+                running: false
+            });
             chai.assert.throws(Timer.prototype.stop.bind(timer), Error);
         });
     });
@@ -167,60 +212,96 @@ describe('Timer', () => {
     describe('Timer.countdown', () => {
         it('Works for interval_ms different from 1000ms', (done) => {
             let timer = new Timer({
-                                hours: 0,
-                                minutes: 0,
-                                seconds: 0,
-                                miliseconds: 15,
-                                interval_ms: 10
-                            });
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 15,
+                interval_ms: 10
+            });
             timer.start();
             setTimeout(() => {
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('hours'), 0, 'hours'); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('minutes'), 0, 'minutes'); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('seconds'), 0, 'seconds'); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('miliseconds'), 0, 'miliseconds'); });
-                catchAsync(done, () => { chai.assert.isFalse(timer.running); });
-                catchAsync(done, () => { chai.assert.isTrue(timer.is_done); });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('hours'), 0, 'hours');
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('minutes'), 0, 'minutes');
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('seconds'), 0, 'seconds');
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('miliseconds'), 0, 'miliseconds');
+                });
+                catchAsync(done, () => {
+                    chai.assert.isFalse(timer.running);
+                });
+                catchAsync(done, () => {
+                    chai.assert.isTrue(timer.is_done);
+                });
                 done();
             }, 20);
         });
 
         it('Stops when reaching 00:00:00:000 and sets the "complete" flag to true', function(done) {
             let timer = new Timer({
-                                hours: 0,
-                                minutes: 0,
-                                seconds: 0,
-                                miliseconds: 10,
-                                interval_ms: 10
-                            });
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 10,
+                interval_ms: 10
+            });
             timer.start();
             setTimeout(() => {
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('hours'), 0, 'hours'); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('minutes'), 0, 'minutes'); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('seconds'), 0, 'seconds'); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('miliseconds'), 0, 'miliseconds'); });
-                catchAsync(done, () => { chai.assert.isFalse(timer.running); });
-                catchAsync(done, () => { chai.assert.isTrue(timer.is_done); });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('hours'), 0, 'hours');
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('minutes'), 0, 'minutes');
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('seconds'), 0, 'seconds');
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('miliseconds'), 0, 'miliseconds');
+                });
+                catchAsync(done, () => {
+                    chai.assert.isFalse(timer.running);
+                });
+                catchAsync(done, () => {
+                    chai.assert.isTrue(timer.is_done);
+                });
                 done();
             }, 10);
         });
 
         it('It uses ms_left instead of interval_ms in case ms_left<interval_ms', (done) => {
             let timer = new Timer({
-                            hours: 0,
-                            minutes: 0,
-                            seconds: 0,
-                            miliseconds: 5,
-                            interval_ms: 100
-                        });
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 5,
+                interval_ms: 100
+            });
             timer.start();
             setTimeout(() => {
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('hours'), 0); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('minutes'), 0); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('seconds'), 0); });
-                catchAsync(done, () => { chai.assert.strictEqual(timer.time.get('miliseconds'), 0); });
-                catchAsync(done, () => { chai.assert.isFalse(timer.running); });
-                catchAsync(done, () => { chai.assert.isTrue(timer.is_done); });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('hours'), 0);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('minutes'), 0);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('seconds'), 0);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('miliseconds'), 0);
+                });
+                catchAsync(done, () => {
+                    chai.assert.isFalse(timer.running);
+                });
+                catchAsync(done, () => {
+                    chai.assert.isTrue(timer.is_done);
+                });
                 done();
             }, 10);
         });
@@ -237,6 +318,80 @@ describe('Timer', () => {
 
         it('Sets the "running" flag to false', () => {
             chai.assert.isFalse(timer.running);
+        });
+    });
+
+    describe('Timer.set', () => {
+        let timer;
+
+        beforeEach(() => {
+            timer = new Timer({
+                hours: 0,
+                minutes: 10,
+                seconds: 15,
+                miliseconds: 5,
+                interval_ms: 10
+            });
+        });
+
+        it('Should update Timer.time values with the ones specified', () => {
+            timer.set({
+                hours: 12,
+                minutes: 19,
+                seconds: 50,
+                miliseconds: 325
+            });
+
+            chai.assert.strictEqual(timer.time.get('hours'), 12);
+            chai.assert.strictEqual(timer.time.get('minutes'), 19);
+            chai.assert.strictEqual(timer.time.get('seconds'), 50);
+            chai.assert.strictEqual(timer.time.get('miliseconds'), 325);
+        });
+    });
+
+    describe('Timer.reset', () => {
+        let timer;
+
+        beforeEach(() => {
+            timer = new Timer({
+                hours: 0,
+                minutes: 10,
+                seconds: 15,
+                miliseconds: 5,
+                interval_ms: 10
+            });
+        });
+
+        it('Should reset Timer.time to values used on instantiation', (done) => {
+            timer.start();
+            setTimeout(() => {
+                timer.reset();
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('hours'), 0);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('minutes'), 10);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('seconds'), 15);
+                });
+                catchAsync(done, () => {
+                    chai.assert.strictEqual(timer.time.get('miliseconds'), 5);
+                });
+                done();
+            }, 20);
+        });
+
+        it('Should stop the countdown', () => {
+            timer.start();
+            timer.reset();
+            chai.assert.isFalse(timer.running);
+        });
+
+        it('Should set Timer.is_done to false', () => {
+            timer.start();
+            timer.reset();
+            chai.assert.isFalse(timer.is_done);
         });
     });
 });
